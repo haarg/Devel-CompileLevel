c4f96ff5f3d618bae7e8787982c1430090c2aff1
Graham Knop <haarg@haarg.org>
2020-03-09 11:28:10 +0100

    fix level check on perl 5.30.2
    The change in 5.31.7 which breaks using the warning bits to check for
    the compile level has been backported to 5.30.2.

--- lib/Devel/CompileLevel.pm
+++ lib/Devel/CompileLevel.pm
@@ -8,7 +8,7 @@ $VERSION =~ tr/_//d;
 
 use Exporter (); BEGIN { *import = \&Exporter::import }
 BEGIN {
-  *_WARNING_BITS_CHECK = "$]" <= 5.031006 ? sub(){1} : sub(){0};
+  *_WARNING_BITS_CHECK = "$]" <= 5.030001 ? sub(){1} : sub(){0};
 }
 
 our @EXPORT_OK = qw(
